f6bd0deb600878c614a2d372acecdac7a3958917
salihgenchw <salihgenc34@gmail.com>
2024-03-06 21:07:47 +0300

    Add GeneralStatusBar component and update Navigation styling

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./src/navigation/Navigation";
+import GeneralStatusBarColor from "./src/components/GeneralStatusBar";
+import COLORS from "./src/constants/colors/Colors";
 
-export default function App() {
+const App = () => {
+  const colors = COLORS.light; // Sonrasında dark mode eklenecek.
+
   return (
     <NavigationContainer>
+      <GeneralStatusBarColor backgroundColor={colors.primary} barStyle="auto" />
       <Navigation />
     </NavigationContainer>
   );
-}
+};
+
+export default App;
--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,33 +1,86 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Dimensions, StyleSheet } from "react-native";
+import COLORS from "../../src/constants/colors/Colors";
 import HomeScreen from "../screens/Home/HomeScreen";
-import CartSreen from "../screens/Cart/CartScreen";
+import CartScreen from "../screens/Cart/CartScreen";
 import FavoritesScreen from "../screens/Favorite/FavoritesScreen";
 import ProfileScreen from "../screens/User/ProfileScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const Navigation = () => {
+  const colors = COLORS.light; // Sonrasında dark mode eklenecek.
+
   return (
     <Tab.Navigator
       initialRouteName="Home"
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: "tomato",
+        tabBarActiveTintColor: colors.primary,
         tabBarInactiveTintColor: "gray",
         tabBarLabelStyle: {
-          fontSize: 12,
+          //   fontSize: Dimensions.get("window").width > 400 ? 14 : 12, Labelleri kaldırdım.
+          display: "none",
         },
         tabBarStyle: {
-          backgroundColor: "white",
+          backgroundColor: colors.background,
         },
       }}
     >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Cart" component={CartSreen} />
-      <Tab.Screen name="Favorites" component={FavoritesScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={CartScreen}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? "cart" : "cart-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? "heart" : "heart-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? "person" : "person-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
