9776a4de0e58e05784a01ebb8b25879a5016181b
salihgenchw <salihgenc34@gmail.com>
2024-03-06 20:30:52 +0300

    add new screens and colors

---
+++ src/constants/colors/Colors.js
@@ -0,0 +1,18 @@
+const lightColors = {
+  primary: "#007AFF",
+  secondary: "#4CD964",
+  background: "#F5F5F5",
+  text: "#000000",
+};
+
+const darkColors = {
+  primary: "#0A84FF",
+  secondary: "#34C759",
+  background: "#1C1C1E",
+  text: "#F5F5F5",
+};
+
+export default {
+  light: lightColors,
+  dark: darkColors,
+};
